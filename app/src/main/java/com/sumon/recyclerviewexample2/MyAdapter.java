e88e6b2a2fd45d75ed7209f31b4317162b3a638e
Sumon <sumonece15@gmail.com>
2021-12-07 21:47:37 +0600

    RecyclerView Example2 (1.0)

--- app/src/main/java/com/sumon/recyclerviewexample2/MyAdapter.java
+++ app/src/main/java/com/sumon/recyclerviewexample2/MyAdapter.java
@@ -9,27 +9,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.holder>
-{
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.holder> {
     String data[];
 
-    public MyAdapter(String[] data)
-    {
+    public MyAdapter(String[] data) {
         this.data = data;
     }
 
     @NonNull
     @Override
-    public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
-        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
-        View view=inflater.inflate(R.layout.single_row,parent,false);
+    public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.single_row, parent, false);
         return new holder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull holder holder, int position)
-    {
+    public void onBindViewHolder(@NonNull holder holder, int position) {
         holder.tv.setText(data[position]);
     }
 
@@ -39,14 +35,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.holder>
     }
 
 
-    class holder extends RecyclerView.ViewHolder
-    {
+    class holder extends RecyclerView.ViewHolder {
         ImageView img;
         TextView tv;
+
         public holder(@NonNull View itemView) {
             super(itemView);
-            img=(ImageView)itemView.findViewById(R.id.img1);
-            tv=(TextView)itemView.findViewById(R.id.t1);
+            img = (ImageView) itemView.findViewById(R.id.img1);
+            tv = (TextView) itemView.findViewById(R.id.t1);
         }
     }
 
